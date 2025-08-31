dcb3c3c0faf23f55daf281c59e01ebcdfd52c4e2
Alexey <erg163@ya.ru>
2025-08-31 13:46:00 +0400

    Initial Test Form fix

--- src/test/java/ru/netology/web/TestaccountRegistrationCompleted.java
+++ src/test/java/ru/netology/web/TestaccountRegistrationCompleted.java
@@ -1,4 +1,46 @@
-package PACKAGE_NAME;
+package ru.netology.web;
+
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.$;
 
 public class TestaccountRegistrationCompleted {
+
+    @BeforeEach
+    void setup() {
+        Selenide.open("http://localhost:9999/");
+    }
+
+    private String generateDate(long addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @Test
+    public void accountRegistrationCompleted() {
+        $("[data-test-id='city'] input").setValue("Самара");
+        String planningDate = generateDate(4, "dd.MM.yyyy");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(planningDate);
+        $("[data-test-id='name'] input").setValue("Иван Иванов");
+        $("[data-test-id='phone'] input").setValue("+79033003333");
+        $("[data-test-id='agreement']").click();
+        $("button.button").click();
+        $(".notification__content")
+                .should(Condition.visible, Duration.ofSeconds(15))
+                .should(Condition.text("Встреча успешно забронирована на " + planningDate));
+
+
+    }
 }
+
+
+
